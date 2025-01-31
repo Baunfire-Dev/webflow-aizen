bb95efbdba8f7748aeafc7771fc8f3a058ba02ac
Rich Philben Cuer <rcuer@baunfire.com>
2025-02-01 06:50:08 +0800

    updates to layout

--- js/custom/script.js
+++ js/custom/script.js
@@ -16,8 +16,8 @@ $(document).ready(function () {
 
         if (navbar.length) {
             entranceTL.fromTo(navbar,
-                { y: -100 },
-                { duration: 1, y: 0 }
+                { y: -100, opacity: 0 },
+                { duration: 1, y: 0, opacity: 1 }
             );
         }
 
